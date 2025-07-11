eac4077e9462aa36e5c00d26bf6f098b269f6ecb
NinjaVass-C <connorvassinfo@gmail.com>
2025-07-10 20:56:48 -0400

    Added Hand.java to make dealer and player classes much cleaner

--- src/Dealer.java
+++ src/Dealer.java
@@ -2,48 +2,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Dealer {
-    private ArrayList<Card> hand = new ArrayList<>();
+    private Hand hand = new Hand("Dealer");
 
     public Dealer() {
     }
 
     public void updateHand(Card[] addedCards) {
-        hand.clear();
-        hand.addAll(Arrays.asList(addedCards));
+        hand.updateHand(addedCards);
     }
 
     // show dealers cards after deal,
     // @todo if cards equal 21, display blackjack.
     public void displayFirst() {
-        System.out.println("Dealer shows " + hand.getFirst());
+        System.out.println("Dealer shows " + hand.getHand().getFirst());
     }
 
     public void printCards() {
-        System.out.print("Dealer has: ");
-        for (Card card : hand) {
-            System.out.print(card + " | ");
-        }
-        System.out.println();
+        hand.printHand();
     }
 
     public int getHandValue() {
-        int total = 0;
-        for (Card card : hand) {
-            total += card.getValue();
-        }
-
-        return total;
+        return hand.getHandValue();
     }
     // After player stands, deal cards.
     public boolean dealToEnd(Deck activeDeck, boolean playerBust) {
         // for now, treat aces as 1. @todo fix for 11 handle
         if (!playerBust) {
             while (this.getHandValue() < 17) {
-                hand.add(activeDeck.dealCard());
-                printCards();
+                this.printCards();
+                hand.addCard(activeDeck.dealCard());
             }
         }
-        printCards();
+        this.printCards();
         return this.getHandValue() > 21;
     }
 }
