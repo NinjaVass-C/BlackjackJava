8493331039748d096370fc4e2ff32e82bb3584ea
NinjaVass-C <connorvassinfo@gmail.com>
2025-07-13 17:48:53 -0400

    Finished adding comments

--- src/Hand.java
+++ src/Hand.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Represents a hand for an individual player or dealer, consists of
+ * an arraylist of cards and manages hand logic.
+ *
+ * Author: Connor Vass
+ * Version: 1.0
+ * Date: July 13, 2025
+ */
 public class Hand {
+    // creates arraylist for cards
     private ArrayList<Card> hand = new ArrayList<>();
     // used to tell difference between dealer/player
     private String identity;
@@ -9,11 +18,22 @@ public class Hand {
         this.identity = identity;
     }
 
+    /**
+     * Used to update hand with starting cards, clears last hand
+     * @param addedCards array of cards initially dealt
+     */
     public void updateHand(Card[] addedCards) {
         hand.clear();
         hand.addAll(Arrays.asList(addedCards));
     }
 
+    /**
+     * Used to compute the value of the users hand
+     * All card values are the same as their rank except
+     * Aces, which can be 1 or 11
+     * @return int value of hand
+     */
+    //@todo fix ace handling to properly change to 1 when needed
     public int getHandValue() {
         int total = 0;
         for (Card card : hand) {
@@ -30,10 +50,18 @@ public class Hand {
         return total;
     }
 
+    /**
+     * Returns the player/dealers hand
+     * @return ArrayList of cards that make up hand
+     */
     public ArrayList<Card> getHand() {
         return hand;
     }
 
+    /**
+     * Function used to print currently shown cards,
+     * Will be removed in FRONT_END
+     */
     public void printHand() {
         System.out.print(identity + " has: ");
         for (Card card : this.hand) {
@@ -42,6 +70,12 @@ public class Hand {
         System.out.println();
     }
 
+    /**
+     * Used to deal a card to the player or dealer,
+     * then check to ensure they are still <= 21
+     * @param card dealt card
+     * @return boolean check if player/dealer busted
+     */
     public boolean addCard(Card card) {
         hand.add(card);
         return this.getHandValue() > 21;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-import java.util.ArrayList;
-// Main class that initializes game object and starts game with passed params.
+/**
+ * Main class for blackjack project
+ * Current use is to create game object, pass initial game parameters,
+ * Then print to the user that the game is over.
+ * This will be changed with BACK_END, but for now will be used for Version 1.0
+ *
+ *
+ * Author: Connor Vass
+ * Version: 1.0
+ * Date: July 13, 2025
+ */
 public class Main {
     public static void main(String[] args) {
         // creates game object
