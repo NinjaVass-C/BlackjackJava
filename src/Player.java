7d00e79bbd1746553ccb1e3c5560541824d15283
NinjaVass-C <connorvassinfo@gmail.com>
2025-07-13 00:56:47 -0400

    Modified ante system for player, added double down mechanic to player class, removed unused variables

--- src/Player.java
+++ src/Player.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Player {
     private Hand hand = new Hand("Player");
     private int chips = 500;
-    private boolean bust = false;
+    private int ante;
     private boolean hasAutoBlackjack = false;
 
     // scanner variable for console input (will be removed with front end)
@@ -21,35 +21,31 @@ public class Player {
         return this.chips;
     }
 
-    public boolean getBust() {
-        return this.bust;
-    }
-
-    public void Win (int winnings) {
+    public void Win () {
         if (hasAutoBlackjack) {
-            chips += (int) Math.floor(winnings + (winnings * 1.5));
+            chips += (int) Math.floor(ante + (ante * 1.5));
         }
         else {
-            chips += winnings * 2;
+            chips += ante * 2;
         }
-        hasAutoBlackjack = false;
     }
 
-    public void Push (int push) {
-        chips += push;
+    public void Push () {
+        chips += ante;
     }
 
     public int Wager() {
+        this.clearConditions();
         while (true) {
             try {
                 System.out.println("What would you like to wager? (Max 500 Min 25 increment 25) (Current Chips: " + chips + ") ");
-                int amt = input.nextInt();
+                ante = input.nextInt();
                 // @todo add global setting for table limits
                 // for now just do typical limits.
-                if (amt >= 25 && amt <= 500 && (amt % 25 == 0) && amt <= chips) {
-                    chips -= amt;
+                if (ante >= 25 && ante <= 500 && (ante % 25 == 0) && ante <= chips) {
+                    chips -= ante;
                     input.nextLine();
-                    return amt;
+                    return ante;
                 } else {
                     throw new Exception("Invalid input");
                 }
@@ -61,8 +57,7 @@ public class Player {
     }
 
     // update the players hand with new cards.
-    // @todo make flow better with new Hand class
-    // @todo payout for blackjack
+    // @todo make flow better with new Hand class (see game.java)
     public void updateHand(Card[] addedCards) {
         hand.updateHand(addedCards);
     }
@@ -82,18 +77,29 @@ public class Player {
     // @todo account for double down
     public boolean turn(Deck activeDeck) {
         while (true) {
-            System.out.println("Would you like to hit or stand?");
+            System.out.println("Would you like to hit, stand, or double down?");
             String response = input.nextLine();
             if (response.equalsIgnoreCase("stand")) {
                 return false;
             } else if (response.equalsIgnoreCase("hit")) {
-                boolean check = this.hit(activeDeck.dealCard());
+                boolean checkBust = this.hit(activeDeck.dealCard());
                 printCards();
-                if (check) {
-                    bust = true;
-                    return bust;
+                if (checkBust) {
+                    return true;
                 }
-            } else {
+            }
+            else if (response.equalsIgnoreCase("double")) {
+                if (chips < ante) {
+                    System.out.println("Not enough chips to double");
+                    continue;
+                }
+                chips -= ante;
+                ante *= 2;
+                boolean checkBust = this.hit(activeDeck.dealCard());
+                printCards();
+                return checkBust;
+            }
+            else {
                 System.out.println("Invalid input");
             }
         }
@@ -102,6 +108,11 @@ public class Player {
     public boolean autoBlackjack() {
         hasAutoBlackjack = hand.getHandValue() == 21;
         return hasAutoBlackjack;
+    }
+
+    public void clearConditions() {
+        ante = 0;
+        hasAutoBlackjack = false;
     }
 
 }
